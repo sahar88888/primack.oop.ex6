1a6114d126ec61d17849eb9b087f000fe20aea67
sahar primack <sahar.primack@gmail.com>
2017-06-17 18:30:00 +0300

    Original structure

--- src/code_elements/Condition.java
+++ src/code_elements/Condition.java
@@ -1,23 +1,33 @@
 package code_elements;
 
+import code_elements.variables.Variable;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by t8417719 on 12/06/2017.
  */
 public abstract class Condition extends Block {
 
-    protected Condition(BufferedReader f, String def_line) throws
+    static public String CREATE_REGEX = "blablabla"; //TODO fix
+
+    protected Condition(BufferedReader f, String def_line,
+            ArrayList<Variable> scope_vars)  throws
             IOException, BadElementException{
-        super(f,def_line);
+        super(f,def_line, scope_vars);
     }
 
-    static Condition createFromLine(BufferedReader f, String line) throws
+    static Condition createFromLine(BufferedReader f, String line,
+            ArrayList<Variable> scope_vars ) throws
             BadElementException, IOException{
-        Condition elem;
-        if((elem = IfCondition.createFromLine(f ,line))==null){
-            elem = WhileCondition.createFromLine(f, line);
+        Condition elem = null;
+        if(CodeElement.check_match(line, CREATE_REGEX)) {
+            if ((elem = IfCondition.createFromLine(f, line, scope_vars))  ==
+                    null) {
+                elem = WhileCondition.createFromLine(f, line, scope_vars);
+            }
         }
         return elem;
     }
@@ -27,5 +37,16 @@ public abstract class Condition extends Block {
         if (e instanceof Method){
             throw new BadElementException();
         }
+    }
+
+    @Override
+    public void is_legal() throws BadElementException {
+        super.is_legal();
+        String[] statements = (this.definition_line.split("\\(")[1]).split
+                ("logical operators");
+        for(String statement : statements){
+            
+        }
+
     }
 }
