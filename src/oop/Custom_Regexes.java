c625467dc8a7f8caae9c94ee4aadb086a70b3bdd
raveh38 <raveh.shulman.38.talpiot@gmail.com>
2017-06-17 17:01:20 +0300

    Check - changed block.

---
+++ src/oop/Custom_Regexes.java
@@ -0,0 +1,9 @@
+package oop;
+
+/**
+ * A library of  custom regexes for our use.
+ */
+ public class Custom_Regexes {
+    public static String VAR_TYPE = "[int|double|String|boolean|char]";
+    public static String NAME = "[^\\W\\d]+[\\w]*";// a pattern for a name. (e.g. A1, Zorg_42).
+}
