0597d34fc5af163827aa6e5568143aa7cb9a5a9b
raveh38 <raveh.shulman.38.talpiot@gmail.com>
2017-06-17 17:05:51 +0300

    Check - changed block.

--- src/oop/Custom_Regexes.java
+++ src/oop/Custom_Regexes.java
@@ -5,5 +5,6 @@ package oop;
  */
  public class Custom_Regexes {
     public static String VAR_TYPE = "[int|double|String|boolean|char]";
-    public static String NAME = "[^\\W\\d]+[\\w]*";// a pattern for a name. (e.g. A1, Zorg_42).
+    public static String VAR_NAME = "[a-zA-Z_][\\w]*";// a pattern for a var name. (e.g. A1, _Zorg_42).
+    public static String METHOD_NAME = "[a-zA-Z][\\w]*";// a pattern for a name. (e.g. A1, print_files
 }
