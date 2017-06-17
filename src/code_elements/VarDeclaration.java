0d0cc95d0d34cb990c81800978a1ef1d5de2b54d
raveh38 <raveh.shulman.38.talpiot@gmail.com>
2017-06-17 17:31:26 +0300

    Check - changed block.

--- src/code_elements/VarDeclaration.java
+++ src/code_elements/VarDeclaration.java
@@ -3,16 +3,30 @@ package code_elements;
 import code_elements.variables.Variable;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import  static oop.Custom_Regexes.*;
+
+
 
 /**
  * Created by t8417719 on 12/06/2017.
  */
 public class VarDeclaration extends Statement {
 
+    static Pattern p = Pattern.compile(VAR_TYPE+"[\\s]+("+VAR_NAME+"\\ [\\s]*,[\\s]*)*");
+
 
     ArrayList<Variable> vars;
 
-    static CodeElement createFromLine(String line){
+    static VarDeclaration createFromLine(String line) throws BadElementException{
         // TODO wild sex
+        Matcher m  = p.matcher(line);
+        if (m.matches())
+        {
+
+        }
+        else
+            return null;
     }
 }
