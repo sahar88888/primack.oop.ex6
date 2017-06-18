79113f6428065c5b3e789c17dab5c54b551a71f5
raveh38 <raveh.shulman.38.talpiot@gmail.com>
2017-06-19 00:17:36 +0300

    Check - changed block.

--- src/code_elements/VarDeclaration.java
+++ src/code_elements/VarDeclaration.java
@@ -1,11 +1,13 @@
 package code_elements;
 
 import code_elements.variables.Variable;
+import code_elements.variables.Variable.*;
 
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import  static oop.Custom_Regexes.*;
+
 
 
 
@@ -17,33 +19,35 @@ public class VarDeclaration extends Statement {
     static String Line_Regex= VAR_TYPE+"[\\s]+("+VAR_NAME+WHITESPACE+","+WHITESPACE+")*"+VAR_NAME+WHITESPACE +
             "("+ASSIGNMENT+")?"+LINE_END;
 
-    private ArrayList<String> vars;//String of variables declared
+    private ArrayList<Variable> vars;//String of variables declared
     private String type;//
 
-    public VarDeclaration(ArrayList<String> vars, String type) {
+    public VarDeclaration(ArrayList<Variable> vars, String type) {
         this.vars = vars;
         this.type = type;
     }
 
-    static VarDeclaration createFromLine(String line){
+    static VarDeclaration createFromLine(String line) throws BadElementException{
         // TODO wild sex
 
         if (line.matches(Line_Regex))
         {
             String[] LineParts = line.split("=",2); //splitting the line to a max of 2 parts: vars and assignment.
 
-            ArrayList<String> var_names = new ArrayList<>();
+            ArrayList<VarType> var_names = new ArrayList<>();
 
             //TAKE CARE OF - DEAL WITH THE CASE OF ASSIGNMENT!
             Pattern p = Pattern.compile(VAR_NAME);
             Matcher m = p.matcher(LineParts[0]);//searching for names in the first part.
 
             m.find();//to skip the variable type, that surely could be a var name.
-            String type = Cut(line,m);
+            String type_str = Cut(line,m);
+            VarType type = GetValueType(type_str);
 
             while (m.find()) //getting all variable names.
             {
-                var_names.add(Cut(line,m));//adding all names to the array.
+                String var_name=Cut(line,m);//adding all names to the array.
+                Variable var = new Variable(var_name,type);
             }
             if(LineParts.length>1){ //if there is also an assignment
 
