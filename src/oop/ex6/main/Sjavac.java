8c10864c521a965835f57be84bd84323bd5d6ad8
raveh38 <raveh.shulman.38.talpiot@gmail.com>
2017-06-24 19:09:05 +0300

    Check - changed block.

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -10,6 +10,9 @@ import java.util.ArrayList;
  * Created by t8417719 on 12/06/2017.
  */
 public abstract class Sjavac {
+    final static String LEGAL = "0";
+    final static String ILLEGAL = "1";
+    final static String ERROR = "2";
     public static void main(String[] args) {
 
         if(args.length!=1){
@@ -17,20 +20,21 @@ public abstract class Sjavac {
             System.err.println("incorrect number of parameters.");
             return;
         }
+
         try {
             FileReader fr = new FileReader(args[0]);
             BufferedReader br = new BufferedReader(fr);
             Program program = new Program(br);
             program.is_legal(new ArrayList<>());
-            System.out.print('0');
+            System.out.print(LEGAL);
         }
         catch(BadElementException e){
             System.err.println(e.getMessage());
-            System.out.print('1');
+            System.out.print(ILLEGAL);
         }
         catch(Exception e){
             System.err.println(e.getMessage());
-            System.out.print('2');
+            System.out.print(ERROR);
         }
     }
 }
